cb60242576ef372a36e14704b4c6442b90b544fa
PedroAlves77 <ps758718@gmail.com>
2024-10-13 10:45:49 -0300

    form modified

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,10 +1,8 @@
-//captura os eventos do formulário
 const form = document.querySelector('.form')
 const popup = document.getElementById('popup')
 const overlay = document.getElementById('overlay')
 const closePopupBtn = document.getElementById('close-popup')
 
-//validar os campos do formulário
 function validateForm() {
     const name = form.querySelector('[name="name"]')
     const email = form.querySelector('[name="email"]')
@@ -12,45 +10,46 @@ function validateForm() {
     const age = form.querySelector('[name="age"]')
     const message = form.querySelector('[name="message"]')
 
-    // verificação dos campos preenchidos
     if (!name.value || !email.value || !phone.value || !age.value || !message.value) {
         alert('Por favor, preencha todos os campos!');
         return false;
     }
+    return true //envia o formulário se tudo estiver validado
+}
 
-    console.log('formulário enviado:');
-    console.log('nome', name.value);
-    console.log('email', email.value);
-    console.log('telefone', phone.value);
-    console.log('idade', age.value);
-    console.log('mensagem', message.value);
+function submitForm(){
+    const formData = new FormData(form);
 
-    return true;
+    fetch('/', {
+        method: 'POST',
+        body: formData
+    })
+    .then(() => {
+        console.log('Formulário enviado com sucesso');
 
+        setTimeout(() =>{
+            popup.style.display = 'block';
+            overlay.style.display = 'block';
+        }, 200);
 
+        form.reset()
+    })
+    .catch((error) => {
+        console.error('Erro ao enviar o formulário:', error);
+    })
 }
 
 
-//previne o comportamento de recarregar a página
 form.addEventListener('submit', function (event) {
     event.preventDefault();
 
-    //valida os dados enviados
     if (validateForm()) {
-        //exibe o popup
-        setTimeout(() => {
-            popup.style.display = 'block';
-            overlay.style.display = 'block'
-        }, 200)
-
-        //reseta o formulário
-        form.reset()
+        submitForm()
     }
 
 
 });
 
-//fecha o popup quando o botão é clicado
 closePopupBtn.addEventListener('click', function () {
     popup.style.display = 'none';
     overlay.style.display = 'none';
